4f4efd0adbc72e0dc9ab971689aff4b237e87a7d
Ravi Patel <rpmd30@gmail.com>
2024-04-11 21:39:35 -0500

    Update ingest and add readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+# Setup steps:
+
+1. Install Docker(https://docs.docker.com/engine/install/)
+2. Install Docker Compose(https://docs.docker.com/compose/install/)
+3. You should have python 3.10 or higher installed
+4. Install virtualenv if you don't already have it
+5. run `virtualenv env`
+6. run `source env/bin/activate`
+7. run `pip install -r requirements/requirements.txt`
+8. install make (not necessary, but makes life easier)
+9. run `make build`
+10. run `make run`
+11. run `make submit app=gather_data.py`
+12. run `make submit app=schema_create.py`
+13. run `make submit app=ingest.py`
+# Projects referenced:
+
 https://medium.com/@MarinAgli1/setting-up-a-spark-standalone-cluster-on-docker-in-layman-terms-8cbdc9fdd14b
 http://snap.stanford.edu/data/web-Amazon-links.html
--- apps/ingest.py
+++ apps/ingest.py
@@ -1,6 +1,7 @@
 import json
 import gzip
 import os
+import traceback
 
 DATA_DIR = "/opt/spark/data/raw_data"
 
@@ -37,33 +38,38 @@ INSERT_PRODUCT_QUERY = (
 
 count = 0
 for file in list_files:
-    for e in parse(os.path.join(DATA_DIR, file)):
-        if not "product/productId" in e:
-            continue
+    try:
+        for e in parse(os.path.join(DATA_DIR, file)):
+            if not "product/productId" in e:
+                continue
 
-        product_id = e["product/productId"]
-        product_title = e["product/title"]
-        product_cateory = file.split(".")[0]
-        review_text = e["review/summary"] + ' ' + e['review/text']
-        review_rating = e["review/score"]
-        helpfulness_split = e["review/helpfulness"].split("/")
-        review_helpfulness = 0
-        try:
-            review_helpfulness = int(helpfulness_split[0]) / int(helpfulness_split[1])
-        except:
-            pass
-        curs.execute(
-            INSERT_PRODUCT_QUERY, (product_id, product_title, 0, product_cateory)
-        )
-        INSERT_REVIEW_QUERY = """INSERT INTO reviews (product_id, helpfulness, rating, summary) VALUES (%s, %s, %s, %s)"""
-        curs.execute(
-            INSERT_REVIEW_QUERY,
-            (product_id, review_helpfulness, review_rating, review_text[:511]),
-        )
-        count += 1
-        if count % 1000 == 0:
-            con.commit()
-            print(f"Inserted {count} rows.")
+            product_id = e["product/productId"]
+            product_title = e["product/title"]
+            product_cateory = file.split(".")[0]
+            review_text = e["review/summary"] + ' ' + e['review/text']
+            review_rating = e["review/score"]
+            helpfulness_split = e["review/helpfulness"].split("/")
+            review_helpfulness = 0
+            try:
+                review_helpfulness = int(helpfulness_split[0]) / int(helpfulness_split[1])
+            except:
+                pass
+            curs.execute(
+                INSERT_PRODUCT_QUERY, (product_id, product_title, 0, product_cateory)
+            )
+            INSERT_REVIEW_QUERY = """INSERT INTO reviews (product_id, helpfulness, rating, summary) VALUES (%s, %s, %s, %s)"""
+            curs.execute(
+                INSERT_REVIEW_QUERY,
+                (product_id, review_helpfulness, review_rating, review_text[:511]),
+            )
+            count += 1
+            if count % 1000 == 0:
+                con.commit()
+                print(f"Inserted {count} rows.")
+    except Exception as e:
+        print(e)
+        print(traceback.format_exception(e))
+        continue
 con.commit()
 curs.close()
 con.close()
