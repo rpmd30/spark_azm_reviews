f4039411e6de2496fcfd9ea2a994de0a3d9c5bcd
Ravi Patel <rpmd30@gmail.com>
2024-04-15 18:41:09 -0500

    Added basic part of speech analysis.

--- apps/process.py
+++ apps/process.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from sparknlp.pretrained import PretrainedPipeline
+from sparknlp.base import *
 
 spark = (
     SparkSession.builder.appName("processor")
@@ -9,6 +11,9 @@ spark = (
     .getOrCreate()
 )
 
+from sparknlp.pretrained import PretrainedPipeline
+
+pipeline = PretrainedPipeline("explain_document_ml")
 
 products = (
     spark.read.format("jdbc")
@@ -26,27 +31,28 @@ product_id = products.select("id").take(10)[-1].id
 print("p_id: ", product_id)
 
 def get_reviews(product_id):
-    reviews = (
-        spark.read.format("jdbc")
-        .option("driver", "com.mysql.cj.jdbc.Driver")
-        .option("url", "jdbc:mysql://mysql-server:3306/product_analysis")
-        .option("numPartitions", 5)
-        .option("query", f"select * from reviews where product_id = {product_id}")
-        .option("user", "root")
-        .option("password", "root")
-        .load()
-    )
-    reviews.show()
+    try:
+        reviews = (
+            spark.read.format("jdbc")
+            .option("driver", "com.mysql.cj.jdbc.Driver")
+            .option("url", "jdbc:mysql://mysql-server:3306/product_analysis")
+            .option("numPartitions", 5)
+            .option("query", f"select * from reviews where product_id = {product_id}")
+            .option("user", "root")
+            .option("password", "root")
+            .load()
+        )
+        reviews.show()
+        results = []
+        for row in reviews.select("summary").collect():
+            print(row)
+            annotations = pipeline.annotate(row.summary)
+            results.append(list(zip(annotations['lemmas'],annotations['pos'])))
+        print(results)
+    except Exception as e:
+        print("Error: ", e)
 
 product_ids = products.select(["id",'title']).collect()
 
 for p_id in product_ids:
-    
     get_reviews(p_id.id)
-    
-# from sparknlp.pretrained import PretrainedPipeline
-
-# explain_document_pipeline = PretrainedPipeline("explain_document_ml")
-# annotations = explain_document_pipeline.annotate("We are very happy about SparkNLP")
-# print(annotations)
-# print(type(annotations))
